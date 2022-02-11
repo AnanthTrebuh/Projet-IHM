67f18b82696026536d37ea682f9e2aae994c6a1e
AnanthTrebuh <xignir@gmail.com>
2022-02-11 11:53:29 +0100

    create activity dvd and string

---
+++ Projet/app/src/main/java/fr/univ_poitiers/tpinfo/cinematech/ActivityDVD.java
@@ -0,0 +1,14 @@
+package fr.univ_poitiers.tpinfo.cinematech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityDVD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dvd);
+    }
+}
--- Projet/app/src/main/java/fr/univ_poitiers/tpinfo/cinematech/MainActivity.java
+++ Projet/app/src/main/java/fr/univ_poitiers/tpinfo/cinematech/MainActivity.java
@@ -1,14 +1,22 @@
 package fr.univ_poitiers.tpinfo.cinematech;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout movieTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        movieTab = findViewById(R.id.TabLayoutMovies);
+
     }
 }
