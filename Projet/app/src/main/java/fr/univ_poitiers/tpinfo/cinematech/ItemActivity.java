7bab6eb89240ac10399f539bd58fe3924d5120ae
vlescorb <81253696+vlescorb@users.noreply.github.com>
2022-02-23 17:54:50 +0100

    a

--- Projet/app/src/main/java/fr/univ_poitiers/tpinfo/cinematech/ItemActivity.java
+++ Projet/app/src/main/java/fr/univ_poitiers/tpinfo/cinematech/ItemActivity.java
@@ -1,11 +1,21 @@
 package fr.univ_poitiers.tpinfo.cinematech;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.RequestQueue;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ItemActivity  extends AppCompatActivity {
     TextView synopsis;
@@ -15,7 +25,8 @@ public class ItemActivity  extends AppCompatActivity {
     TextView dateExit;
     TextView title;
     TextView scenario;
-    TextView actorPrincipal;
+
+    Button addToWatch;
 
     RequestQueue queue;
 
@@ -30,15 +41,46 @@ public class ItemActivity  extends AppCompatActivity {
         this.actors = findViewById(R.id.actorsMovie);
         this.dateExit = findViewById(R.id.dateExitMovie);
         this.title = findViewById(R.id.titleMovie);
-        this.actorPrincipal = findViewById(R.id.actorsPrincipalMovie);
         this.scenario = findViewById(R.id.scenarioMovie);
+        this.addToWatch = findViewById(R.id.buttonAddListMovie);
 
-        this.synopsis.setText(jm.);
+        addToWatch.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("CinemaTech", Context.MODE_PRIVATE );
+                        SharedPreferences.Editor e = sharedPreferences.edit();
+                        String name = sharedPreferences.getString("Active_Profile","default");
+                        Set<String> movieList = sharedPreferences.getStringSet(name+"_movie", new HashSet<String>());
+                        try {
+                            movieList.add(jm.getTitle());
+                        } catch (JSONException jsonException) {
+                            jsonException.printStackTrace();
+                        }
+                        e.putStringSet(name+"_movie", movieList);
+                        e.apply();
 
 
+                    }
+                }
+        );
 
+        try {
+            this.synopsis.setText(jm.getOverview());
+            this.realisation.setText(jm.getRealisator());
+            this.time.setText(jm.getRunTime());
+            ArrayList<String> actors = jm.getCharacters();
+            String actorsString = "";
+            for(String s : actors)
+                actorsString += ", " + s;
+            this.actors.setText(actorsString);
+            this.dateExit.setText(jm.getReleaseDate());
+            this.title.setText(jm.getTitle());
+            this.scenario.setText(jm.getWriter());
 
-
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
 
     }
